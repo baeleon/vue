2f600bb3be7eabb3dc23fbfc81c86c958eadfa7e
Шомуродов <cometa11we@gmail.com>
2025-07-09 20:53:57 +0500

    add: vue.js 3 + storybook

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Button from "@/stories/components/Buttons/Button.vue";
+import Header from "@/stories/components/Header/Header.vue";
+import router from "./router";
+
+const clickFunction = () => {
+      console.log("hello world");
+};
+</script>
+
+<template>
+      <Header />
+      <RouterView />
+      <Button size="small" :primary="true" title="Button" :onclick="clickFunction" />
+      <Button size="small" title="Button 2" :onClick="() => router.push('/about')" />
+</template>
+
+<style scoped></style>
