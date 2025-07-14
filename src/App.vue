3fcb19f5e934689bc3f751962089b3df2a7d41d9
Шомуродов <cometa11we@gmail.com>
2025-07-15 01:47:49 +0500

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import Button from "@/stories/components/Buttons/Button.vue";
-import Header from "@/stories/components/Header/Header.vue";
 import router from "./router";
 
 const clickFunction = () => {
@@ -11,8 +9,6 @@ const clickFunction = () => {
 <template>
       <Header />
       <RouterView />
-      <Button size="small" :primary="true" title="Button" :onclick="clickFunction" />
-      <Button size="small" title="Button 2" :onClick="() => router.push('/about')" />
 </template>
 
 <style scoped></style>
